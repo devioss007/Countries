276a53e206cdb87a844d624e4d607f18ad359a9d
devioss007 <zekh7777@gmail.com>
2023-05-03 12:23:38 +0500

    first commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Zekhniddin Jumaev on 03/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
